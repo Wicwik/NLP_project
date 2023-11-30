68361ce7f4817c4b6545a73d43af7ad37cdee10a
Robert Belanec <belanecrobert22@gmail.com>
2023-11-30 13:25:59 +0100

    added more metrics

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,30 +1,14 @@
 import numpy as np
-import sklearn.metrics
 import torch
+
+from .utils import normalize_squad, qa_metrics
 
 from torchmetrics import Metric
 from torchmetrics.functional import confusion_matrix
 
-# some are reused from https://github.com/AkariAsai/ATTEMPT/blob/main/attempt/metrics/metrics.py
+from typing import Union, List
 
-def f1_score_with_invalid(predictions, targets) -> dict:
-    """Computes F1 score,  with any prediction != 0 or 1 is counted as incorrect.
-    Args:
-      targets: list of targets, either 0 or 1
-      predictions: list of predictions, any integer value
-    Returns:
-      F1 score, where any prediction != 0 or 1 is counted as wrong.
-    """
-    def binary_reverse(labels):
-        return ['0' if label == '1' else '1' for label in labels]
-    targets, predictions = np.asarray(targets), np.asarray(predictions)
-    # Get indices of invalid predictions.
-    invalid_idx_mask = np.logical_and(predictions != '0', predictions != '1')
-    # For any prediction != 0 or 1, we set the prediction to the opposite of its corresponding target. NOT a mistake, they are using strings because the model returns strings of 1 and 0
-    predictions[invalid_idx_mask] = binary_reverse(targets[invalid_idx_mask])
-    targets = targets.astype(np.int32)
-    predictions = predictions.astype(np.int32)
-    return {"f1": 100 * sklearn.metrics.f1_score(targets, predictions)}
+# some are reused from https://github.com/AkariAsai/ATTEMPT/blob/main/attempt/metrics/metrics.py
 
 # compute a f1 score from a list of strings
 class F1ScoreWithInvalid(Metric):
@@ -90,3 +74,25 @@ class Accuraccy(Metric):
 
     def compute(self):
         return 100 * self.correct.float() / self.total
+    
+
+class SquadMetric(Metric):
+    def __init__(self):
+        super().__init__()
+        self.add_state("targets", default=[], dist_reduce_fx="cat")
+        self.add_state("preds", default=[], dist_reduce_fx="cat")
+
+
+    def update(self, preds, targets):
+        if type(targets[0]) is list:
+            targets = [[normalize_squad(t) for t in u] for u in targets]
+        else:
+            targets = [[normalize_squad(u)] for u in targets]
+
+        preds = [normalize_squad(p) for p in preds]
+
+        self.targets += targets
+        self.preds += preds
+
+    def compute(self):
+        return qa_metrics(self.targets, self.preds)
--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
-# we can try to fetch tasks with this :)
-class TaskFetch:
-    def __init__(self):
-        pass
+import regex as re
+
+def pad_punctuation(text):
+    """Re-implementation of _pad_punctuation in t5. This function adds spaces
+    around punctuation. While this pads punctuation as expected, it has the 
+    unexpected effected of padding certain unicode characters with accents, with
+    spaces as well. For instance: "François" becomes "Fran ç ois"""
+    # Pad everything except for: underscores (_), whitespace (\s),
+    # numbers (\p{N}), letters (\p{L}) and accent characters (\p{M}).
+    text = re.sub(r'([^_\s\p{N}\p{L}\p{M}])', r' \1 ', str(text))
+    # Collapse consecutive whitespace into one space.
+    text = re.sub(r'\s+', ' ', text)
+    return text
