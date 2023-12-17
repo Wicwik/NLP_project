ec3c0a91d56eff4840e62297a9c9e45d0b0e92cf
Robert Belanec <belanecrobert22@gmail.com>
2023-12-17 14:07:47 +0100

    debug

--- pipelines.py
+++ pipelines.py
@@ -141,6 +141,8 @@ class peft_training_pipeline:
             pin_memory=True,
         )
 
+        print(train_dataset)
+
         return train_dataloader, valid_dataloader, test_dataloader
 
     # create torchmetric metrics with their names
